b38d9560f5756b498a1a907141ae88a3d87ddaa8
IVÁN_P\Iván <ivan120695@gmail.com>
2019-01-25 17:53:04 +0100

    Primero ejercicios

---
+++ src/AceptaElReto/Problema114.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package AceptaElReto;
+
+import java.util.Scanner;
+
+/**
+ * @date 25 ene. 2019
+ * 
+ * @author Iván - IvanPerez9
+ *
+ */
+public class Problema114 {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int n = Integer.parseInt(entrada.nextLine());
+		int[] guardar = new int[n];
+		for (int i = 0; i < n; i++) {
+			int num = entrada.nextInt();
+		    int fac=1;
+			for (int j=1;j<=num ;j++){
+				fac *= j;
+			}
+			System.out.println(fac % 10);
+			//guardar[i] = (fac % 10);
+		}
+//		for (int i : guardar) {
+//			System.out.println(i);
+//		}
+	}
+}
---
+++ src/AceptaElReto/Problema145.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package AceptaElReto;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+/**
+ * @date 25 ene. 2019
+ * 
+ * @author Iván - IvanPerez9
+ *
+ */
+public class Problema145 {
+
+	static Scanner in;
+    public static void caso() {
+        String tren = in.nextLine();
+        Deque<Character> pilaHombres = new ArrayDeque<>();
+        int parejas = 0;
+        for (int i = 0; i < tren.length(); i++) {
+            char c = tren.charAt(i);
+            switch(c) {
+                case 'H':
+                case 'h':
+                    pilaHombres.push(c); // Los hombres van a una pila. Solo se puede emparejar el último
+                    break;
+                case '@':
+                    pilaHombres.clear(); // Los hombres no pueden pasar.
+                    break;
+                case 'M':
+                    if (!pilaHombres.isEmpty()) {
+                        if (pilaHombres.pop().equals('H')) { //Si ha llegado un hombre alto
+                            parejas++; // se empareja y los otros hombres siguen avanzando
+                        } else {
+                            pilaHombres.clear(); // Ningun hombre puede avanzar. La pareja los bloquea
+                        }
+                    }
+                    break;
+                case 'm': // idem para mujer baja
+                    if (!pilaHombres.isEmpty()) {
+                        if (pilaHombres.pop().equals('h')) {
+                            parejas++;
+                        } else {
+                            pilaHombres.clear();
+                        }
+                    }       
+            }
+        }
+        System.out.println(parejas);
+    }
+
+    public static void main(String[] args) {
+        in = new Scanner(System.in);
+        while (in.hasNextLine()) {
+            caso();
+        }
+    }
+    
+	
+}

5ecfbd9d540b039499d8f812b8f41e730c0dd39f
IVÁN_P\Iván <ivan120695@gmail.com>
2019-02-07 14:23:14 +0100

    Solucionado

--- src/aceptaElReto/Problema214.java
+++ src/aceptaElReto/Problema214.java
@@ -7,38 +7,13 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 /**
-<<<<<<< HEAD
- * @date 2 feb. 2019
- * 
- * @author Iván - IvanPerez9
- *
-=======
  * @author Ivan.Perez
  *
  *  https://github.com/IvanPerez9
->>>>>>> 92f2f2ba17adb9d9b2460818582e699a362c459f
  */
 public class Problema214 {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		Scanner entrada = new Scanner (System.in);
-		int numero = entrada.nextInt();
-		
-		String linea;
-		HashMap<String, Integer> reyes = new HashMap<String, Integer>();
-		
-		while (numero > 0) {
-			for (int i = 0; i < numero; i++) {
-				linea = entrada.next();
-				if (reyes.containsKey(linea)) {
-					reyes.put(linea, +1);
-				}else {
-					reyes.put(linea, 1);
-				}
-			}
-		}
-=======
 		Scanner entrada = new Scanner(System.in);
 		int num = entrada.nextInt();
 		
@@ -70,7 +45,6 @@ public class Problema214 {
 			num = entrada.nextInt();
 		}
 		entrada.close();
->>>>>>> 92f2f2ba17adb9d9b2460818582e699a362c459f
 	}
 	
 }

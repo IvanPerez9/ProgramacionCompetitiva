8683c1e73e1adad7b0e7224f03234b172b7ae958
Iván Pérez Huete <ivan120695@gmail.com>
2019-02-23 13:14:52 +0100

    Fix 185 and 268

--- src/aceptaElReto/Problema185.java
+++ src/aceptaElReto/Problema185.java
@@ -3,9 +3,9 @@
  */
 package aceptaElReto;
 
-import java.util.HashSet;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.Scanner;
-import java.util.Set;
 
 /**
  * @author Ivan.Perez
@@ -27,34 +27,64 @@ public class Problema185 {
 	   */
 	
 	public static void main(String[] args) {
-		
-		Scanner entrada = new Scanner(System.in);
-		int num = entrada.nextInt();
-		Set<String> conjuntoSI = new HashSet<>();  
-		Set<String> conjuntoNO = new HashSet<>(); 
-		
-		while (num != 0) {
-			for (int i = 0; i < num; i++) {
-				String booleanStr = entrada.next();
-				String ingrediente;
-				if (booleanStr.startsWith("SI:")) {
-					while (!entrada.equals("FIN")) {
-						ingrediente = entrada.next();
-						entrada.next();
-						conjuntoSI.add(ingrediente);
-					}	
-				}else {
-					while (!entrada.equals("FIN")) {
-						ingrediente = entrada.next();
-						entrada.next();
-						if (!conjuntoSI.contains(ingrediente)) {
-							conjuntoNO.add(ingrediente);
-						}
-					}
-				}
-			}
-			System.out.println(conjuntoNO);
-			num = entrada.nextInt();
-		}
+	    Scanner entrada = new Scanner(System.in);
+	    int num = entrada.nextInt();
+	    LinkedList<String> conjuntoSI = new LinkedList<>();  
+	    LinkedList<String> conjuntoNO = new LinkedList<>();
+	    
+	    while (num != 0) {
+	      for (int i = 0; i < num; i++) {
+	        String gustar = entrada.next();
+	        String ingrediente = entrada.next();
+	        if (gustar.equals("SI:")) {
+	          while (!ingrediente.equals("FIN")) {
+	            conjuntoSI.add(ingrediente);
+	            for (int j = 0; j < conjuntoNO.size(); j++) {
+	              if (conjuntoNO.get(j).equals(ingrediente)) {
+	                conjuntoNO.remove(j);
+	              }
+	            }
+	            ingrediente = entrada.next();
+	          }  
+	        }else {
+	          while (!ingrediente.equals("FIN")) {
+	            boolean estaLista = false;
+	            boolean estaListaAux = false;
+	            
+	            for (String s : conjuntoSI) {
+	              if (s.equals(ingrediente)) {
+	                estaLista = true;
+	              }
+	            }
+	            
+	            for (String s : conjuntoNO) {
+	              if (s.equals(ingrediente)) {
+	                estaListaAux = true;
+	              }
+	            }
+	            
+	            if (!estaLista && !estaListaAux) {
+	              conjuntoNO.add(ingrediente);
+	            }
+	            
+	            ingrediente = entrada.next();
+	          }
+	        }
+	      }
+	      Collections.sort(conjuntoNO);
+	      StringBuilder sb = new StringBuilder();
+	      for (String aux : conjuntoNO)
+	        sb.append(aux + " ");
+	      // Borro el ultimo espacio en blanco
+	      if (sb.length() > 0)
+	        sb.delete(sb.length() - 1, sb.length());
+	      String salida = sb.toString();
+	      System.out.println(salida);
+	      
+	      conjuntoNO.clear();
+	      conjuntoSI.clear();
+	      num = entrada.nextInt();
+	    }
+	    entrada.close();
+	  }
 	}
-}

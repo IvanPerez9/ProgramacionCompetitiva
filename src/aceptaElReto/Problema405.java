b96bc00987c96c7cbe4ae5a2999e445fca49bac1
Iván Pérez Huete <ivan120695@gmail.com>
2019-03-19 20:33:51 +0100

    Problema 405

---
+++ src/aceptaElReto/Problema405.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package aceptaElReto;
+
+import java.util.Scanner;
+
+/**
+ * @author Ivan.Perez
+ *
+ *  https://github.com/IvanPerez9
+ */
+public class Problema405 {
+	
+	/* 
+     * Acepta el reto 405
+     * 
+     * 
+     */
+  public static void main(String[] args) {
+	  Scanner entrada = new Scanner(System.in);
+	  int numero, aux , aux1;
+	  boolean condicion ;
+	  
+	  while(true) {
+		  numero = entrada.nextInt();
+		  if (numero == 0) break;
+		  condicion = false;
+		  aux1 = numero;
+		  while(true) {
+			  aux = entrada.nextInt();
+			  if (aux1 == aux -1) {
+				  condicion = true;		  
+			  } else {
+				  if (condicion) {
+					  System.out.print(numero + "-" + aux1);
+					  numero = aux;
+					  condicion = false;
+					  if(aux != 0) System.out.print(",");
+				  } else {
+					  System.out.print(aux1);
+					  numero = aux;
+					  if(aux != 0) System.out.print(",");
+				  }
+			  }
+			  aux1 = aux;
+			  if (aux==0) break;
+		  }
+		  System.out.println();
+	  }
+	  entrada.close();
+  }
+}
